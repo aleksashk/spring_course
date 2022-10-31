086aa19dc24b81d3175e59b63f6d2375460d74f4
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-31 21:37:58 +0300

    init() & destroy()

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,7 +1,7 @@
 package spring_introduction;
 
 public class Dog implements Pet {
-    private String name;
+//    private String name;
 
     public Dog() {
         System.out.println("Dog bean is created");
@@ -11,12 +11,20 @@ public class Dog implements Pet {
     public void say() {
         System.out.println("Bow-Wow");
     }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
-    public String getName() {
-        return name;
+    public void init() {
+        System.out.println("Class Dog: init method");
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void destroy() {
+        System.out.println("Class Dog: destroy method");
     }
 }
--- src/spring_introduction/Test4.java
+++ src/spring_introduction/Test4.java
@@ -6,13 +6,13 @@ public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
 
-        Dog myDog = context.getBean("myPet", Dog.class);
+//        Dog myDog = context.getBean("myPet", Dog.class);
 //        myDog.setName("White");
-        Dog yourDog = context.getBean("myPet", Dog.class);
+//        Dog yourDog = context.getBean("myPet", Dog.class);
 //        yourDog.setName("Black");
-        System.out.println("myDog == yourDog: " + (myDog == yourDog));
-        System.out.println(myDog);
-        System.out.println(yourDog);
+//        System.out.println("myDog == yourDog: " + (myDog == yourDog));
+//        System.out.println(myDog);
+//        System.out.println(yourDog);
 //        System.out.println(myDog.getName());
 //        System.out.println(yourDog.getName());
 
