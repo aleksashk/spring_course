42e063f658b770775e7cc717a5be6c293aa639c7
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-31 22:30:00 +0300

    Configuration with javaCode

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -26,12 +26,12 @@ public class Dog implements Pet {
 //    public void setName(String name) {
 //        this.name = name;
 //    }
-    @PostConstruct
-    public void init() {
-        System.out.println("Class Dog: init method");
-    }
-    @PreDestroy
-    public void destroy() {
-        System.out.println("Class Dog: destroy method");
-    }
+
+//    public void init() {
+//        System.out.println("Class Dog: init method");
+//    }
+//
+//    public void destroy() {
+//        System.out.println("Class Dog: destroy method");
+//    }
 }
