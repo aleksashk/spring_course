d01bd1ff6e296bcaf856fb3eb8a183323d09a537
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-31 21:24:05 +0300

    singleton scope for stateful object

--- src/spring_introduction/Test4.java
+++ src/spring_introduction/Test4.java
@@ -7,14 +7,14 @@ public class Test4 {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
 
         Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("White");
+//        myDog.setName("White");
         Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Black");
-//        System.out.println("myDog == yourDog: " + (myDog == yourDog));
-//        System.out.println(myDog);
-//        System.out.println(yourDog);
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
+//        yourDog.setName("Black");
+        System.out.println("myDog == yourDog: " + (myDog == yourDog));
+        System.out.println(myDog);
+        System.out.println(yourDog);
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
 
         context.close();
     }
