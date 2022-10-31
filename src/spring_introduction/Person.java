8cb768e8a539df083d2f624e45f0732a59f7b2c8
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-31 20:55:21 +0300

    dependency injection with setMethod and applicationContext file

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -3,12 +3,24 @@ package spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
-        this.pet = pet;
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+    public Person() {
+        System.out.println("Person bean is created");
     }
 
-    public void callYourPet(){
+    public void callYourPet() {
         System.out.println("Hello, my lovely PET!!!");
         pet.say();
+    }
+
+    //pet -> setPet
+    //Pet -> setPet
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: SET pet");
+        this.pet = pet;
     }
 }
