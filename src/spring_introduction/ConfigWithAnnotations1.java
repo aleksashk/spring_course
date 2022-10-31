a199ce304bf9cde0570153b2b769dbb3dc3139f9
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-31 21:48:24 +0300

    configuration with annotations @Component with default ID

--- src/spring_introduction/ConfigWithAnnotations1.java
+++ src/spring_introduction/ConfigWithAnnotations1.java
@@ -6,7 +6,7 @@ public class ConfigWithAnnotations1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
 
-        Cat myCat = context.getBean("catBean", Cat.class);
+        Cat myCat = context.getBean("cat", Cat.class);
         myCat.say();
 
         context.close();
