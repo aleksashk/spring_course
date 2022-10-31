7a74f4fadc262042945ee5a1a20baa489815b92d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-31 22:39:16 +0300

    Configuration with javaCode second part

--- src/spring_introduction/Test6.java
+++ src/spring_introduction/Test6.java
@@ -6,8 +6,12 @@ public class Test6 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
-        Pet cat = context.getBean("catBean", Pet.class);
-        cat.say();
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
+
+//        Pet cat1 = context.getBean("catBean", Pet.class);
+//        Pet cat2 = context.getBean("catBean", Pet.class);
+//        cat.say();
 //        Person person = context.getBean("personBean", Person.class);
 //        person.callYourPet();
 
