0de3a76b4a818a2adadc351ca575c03fbd2b8b53
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-31 20:44:04 +0300

    dependency injection with constructor

--- src/spring_introduction/Test3.java
+++ src/spring_introduction/Test3.java
@@ -5,9 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Pet pet = context.getBean("myPet", Pet.class);
+//        Pet pet = context.getBean("myPet", Pet.class);
 //        Pet pet = new Dog();
-        Person person = new Person(pet);
+        Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
     }
 }
