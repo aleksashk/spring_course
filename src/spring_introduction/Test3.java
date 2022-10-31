a890f00c7cc4837f1c4e898ca51f57271158e1d5
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-31 20:39:44 +0300

    test 3 WITH dependency injection

--- src/spring_introduction/Test3.java
+++ src/spring_introduction/Test3.java
@@ -1,8 +1,12 @@
 package spring_introduction;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test3 {
     public static void main(String[] args) {
-        Pet pet = new Dog();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet pet = context.getBean("myPet", Pet.class);
+//        Pet pet = new Dog();
         Person person = new Person(pet);
         person.callYourPet();
     }
