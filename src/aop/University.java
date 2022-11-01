e7d2251b1a6cdceeb9818954bdce8727b8c53881
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-01 09:06:14 +0300

    afterReturningAdvice

---
+++ src/aop/University.java
@@ -0,0 +1,28 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+import java.awt.dnd.DragGestureEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class University {
+    private List<Student> students = new ArrayList<>();
+
+    public void addStudents() {
+        Student st1 = new Student("Aleksandr Philimonov", 4, 7.3);
+        Student st2 = new Student("Dmitry Medvedev", 3, 9.58);
+        Student st3 = new Student("Viacheslav Mishustin", 2, 8.74);
+
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+    }
+
+    public List<Student> getStudents(){
+        System.out.println("Information from method getStudents: ");
+        System.out.println(students);
+        return students;
+    }
+}
--- src/aop/aspects/MyPointCuts.java
+++ src/aop/aspects/MyPointCuts.java
@@ -3,6 +3,6 @@ package aop.aspects;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class MyPointCuts {
-    @Pointcut("execution(* add*(..))")
+    @Pointcut("execution(* asdf*(..))")
     public void allAddMethods(){}
 }
