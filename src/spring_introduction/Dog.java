7babe17f7efaaf0ba22658f235e80846d2db7c8c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-01 06:10:11 +0300

    AOP

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -2,8 +2,8 @@ package spring_introduction;
 
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+//import javax.annotation.PostConstruct;
+//import javax.annotation.PreDestroy;
 
 //@Component
 public class Dog implements Pet {
