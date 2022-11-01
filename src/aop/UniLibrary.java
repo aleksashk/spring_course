cf0fcbbe97b7db1b07719975d7da865b44f509a1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-01 06:58:34 +0300

    beforeGetLoggingAdvice() beforeGetSecurityAdvice()

--- src/aop/UniLibrary.java
+++ src/aop/UniLibrary.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class UniLibrary extends AbstractLibrary {
 
-    public void getBook(Book book) {
-        System.out.println("Мы берём книгу из UniLibrary " + book.getName());
+    public void getBook() {
+        System.out.println("Мы берём книгу из UniLibrary ");
     }
 
     public void getMagazine() {
---
+++ src/aop/aspects/LoggingAndSecurityAspect.java
@@ -0,0 +1,20 @@
+package aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAndSecurityAspect {
+
+    @Before("execution(* get*())")
+    public void beforeGetLoggingAdvice() {
+        System.out.println("beforeGetLoggingAdvice: попытка получить книгу/журнал");
+    }
+
+    @Before("execution(* get*())")
+    public void beforeGetSecurityAdvice() {
+        System.out.println("beforeGetSecurityAdvice: проверка прав на получение книги/журнала");
+    }
+}
