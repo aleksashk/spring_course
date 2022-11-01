682433a54701166dbbb9414fe5a4bd2ee548d75d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-01 20:19:19 +0300

    Session

--- src/hibernate_test/Test1.java
+++ src/hibernate_test/Test1.java
@@ -1,6 +1,7 @@
 package hibernate_test;
 
 import hibernate_test.entity.Employee;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -10,5 +11,7 @@ public class Test1 {
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
     }
 }
