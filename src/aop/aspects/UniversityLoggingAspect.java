6883cac8991c58c22d62b2f64bc382c78198c56e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-01 09:24:53 +0300

    @After

--- src/aop/aspects/UniversityLoggingAspect.java
+++ src/aop/aspects/UniversityLoggingAspect.java
@@ -1,5 +1,6 @@
 package aop.aspects;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -29,8 +30,14 @@ public class UniversityLoggingAspect {
 //                "списка студентов после работы метода getStudents");
 //    }
 
-    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
-    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception) {
-        System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс исключения " + exception);
+//    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
+//    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception) {
+//        System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс исключения " + exception);
+//    }
+
+    @After("execution(* getStudents())")
+    public void afterGetStudentsLoggingAdvice(){
+        System.out.println("afterGetStudentsLoggingAdvice: логируем нормальное " +
+                "окончание работы метода или выброс исключения");
     }
 }
