c91b41ae2be0f8a93b85abcf412112c362d619bc
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-02 16:37:40 +0300

    One-to-One relation getDetail by ID (id doesn't exist)

--- src/hibernate_test2/Test1.java
+++ src/hibernate_test2/Test1.java
@@ -1,6 +1,7 @@
 package hibernate_test2;
 
-import hibernate_test.entity.Employee;
+import hibernate_test2.entity.Detail;
+import hibernate_test2.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,16 +12,42 @@ public class Test1 {
         try (SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
                 .buildSessionFactory()) {
-            Session session;
-            session = factory.getCurrentSession();
+//            Session session = factory.getCurrentSession();
+//
+//            Employee employee = new Employee("Aleks", "Philimonov", "IT", 400);
+//            Detail detail = new Detail("Minsk", "1245789895", "aleksandrphilimonov@gmail.com");
+//
+//            employee.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("\nDONE!!!");
 
-            Employee emp = new Employee("Aleksandr", "Boris", "IT", 470);
+//            Session session = factory.getCurrentSession();
+//
+//            Employee employee = new Employee("Boris", "Jonson", "UK", 500);
+//            Detail detail = new Detail("Lombon", "98985545", "bJonson@rambler.ru");
+//
+//            employee.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("\nDONE!!!");
+
+            Session session = factory.getCurrentSession();
+
             session.beginTransaction();
-            session.save(emp);
+            Employee emp = session.get(Employee.class, 10);
+            System.out.println(emp.getEmpDetail());
+
             session.getTransaction().commit();
-            System.out.println("Done!!!");
-            System.out.println(emp);
+            System.out.println("\nDONE!!!");
         }
     }
 }
--- src/hibernate_test2/entity/Detail.java
+++ src/hibernate_test2/entity/Detail.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table
+@Table(name = "details")
 public class Detail {
 
     @Id
