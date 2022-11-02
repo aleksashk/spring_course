313890cd320f29472ce0a8d45cc6d94dddbe2c88
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-02 21:27:55 +0300

    one-to-many initial commit

---
+++ src/hibernate_one_to_many_bi/Test1.java
@@ -0,0 +1,53 @@
+package hibernate_one_to_many_bi;
+
+import hibernate_one_to_one.entity.Detail;
+import hibernate_one_to_one.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+             Session session = factory.getCurrentSession();) {
+//            Session session = factory.getCurrentSession();
+//
+//            Employee employee = new Employee("Aleks", "Philimonov", "IT", 400);
+//            Detail detail = new Detail("Minsk", "1245789895", "aleksandrphilimonov@gmail.com");
+//
+//            employee.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("\nDONE!!!");
+
+//            Session session = factory.getCurrentSession();
+//
+//            Employee employee = new Employee("Boris", "Jonson", "UK", 500);
+//            Detail detail = new Detail("Lombon", "98985545", "bJonson@rambler.ru");
+//
+//            employee.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("\nDONE!!!");
+
+            session.beginTransaction();
+            Employee emp = session.get(Employee.class, 2);
+
+            session.delete(emp);
+
+            session.getTransaction().commit();
+            System.out.println("\nDONE!!!");
+        }
+    }
+}
