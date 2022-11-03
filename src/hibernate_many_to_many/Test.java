1ad77726380cf7e8de98a32fe0e7584c655f7c7f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-03 21:36:55 +0300

    initial commit Many-To-Many

---
+++ src/hibernate_many_to_many/Test.java
@@ -0,0 +1,27 @@
+package hibernate_many_to_many;
+
+import hibernate_many_to_many.entity.Child;
+import hibernate_many_to_many.entity.Section;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test {
+    public static void main(String[] args) {
+        try (SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Child.class)
+                .addAnnotatedClass(Section.class)
+                .buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
+
+
+            session.beginTransaction();
+
+
+            session.close();
+
+            System.out.println("DONE!!!");
+        }
+    }
+}
